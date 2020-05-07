e87a4bb3173981ba669ec5b5b6d2c873b5d65bdf
scarfaceshing <edrianjansolatorio@gmail.com>
2020-05-07 13:31:15 +0800

    push

--- skillplus/resources/views/_revise/script.blade.php
+++ skillplus/resources/views/_revise/script.blade.php
@@ -1,18 +1,52 @@
 <script src="{{ asset('assets/_plugins/jquery.scrolling-tabs.min.js') }}"></script>
- <script>
+<script>
     $(document).ready(function() {
-        setTimeout(() => {
-           let table =  $('#datatable-details');
+        setTimeout(function() {
+             $('#datatable-details').dtcustom({});
+        }, 500);
 
-           table.DataTable({
-               "responsive": true,
-               "columnDefs": [
+        $(".table-responsive").removeClass();
+    });
+
+    (function($) {
+
+        $.fn.dtcustom = function(option) {
+
+            console.log($(this).parent("div").prepend(`
+            <div class="row">
+                <div class="col-lg-6">
+                        <label class="col-lg-2 control-label">Search</label>
+                        <input class="col-lg-4 form-control" type="text" id="search" />   
+                    </div>
+                    <div class="col-lg-6">
+                        <select class="col-lg-2 form-control" id="limit"> 
+                            <option>10</option>
+                            <option>50</option>
+                            <option>100</option>
+                        </select>
+                    </div>
+            </div>
+            `));
+            
+            var settings = $.extend({
+                responsive: true,
+                searching: false,
+                lengthChange: false,
+                columnDefs: [
                     { responsivePriority: 1, targets: 0 },
                     { responsivePriority: 2, targets: -1 }
                ]
+            }, option);
+
+            return this.DataTable({
+                lengthChange: settings.lengthChange,
+                searching: settings.searching,
+               responsive: settings.responsive,
+               columnDefs: settings.columnDefs
            });
 
-           $(".table-responsive").removeClass();
-        }, 500);
-    });
+        }
+
+    }(jQuery));
+
  </script>
