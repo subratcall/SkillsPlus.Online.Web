400c439b86d031d009975df7dc2a488368dd9fcb
unknown <Administrator@DL-ITDPWS09.dpotmh.local>
2020-05-29 23:47:46 +0800

    fix quiz

--- skillplus/resources/views/student/lesson/quiz.blade.php
+++ skillplus/resources/views/student/lesson/quiz.blade.php
@@ -1,6 +1,7 @@
 @extends('admin.newlayout.layout',['breadcom'=>['Lesson','Edit']])
 @section('title')
-<p id="titleHeader">Quiz</p>
+<a href="/admin/user_student/student_lesson_list/{{request()->route('lid')}}" class="btn btn-warning btn-sm">Back</a>
+Quiz
 @endsection
 
 @section('style')
@@ -48,19 +49,10 @@
     <div class="card-body">
         <div class="row">
             <div class="col-lg-12">
-            <form id="form" method="post" class="form-horizontal">
-                <!-- <input type="hidden" name="id" id="id">
-                <div class="form-group">
-                    <div class="form-group">
-                        <label class="col-md-2 control-label" for="inputDefault">Title</label>
-                        <div class="col-md-10">
-                            <input type="hidden" name="id" id="id" class="form-control">
-                            <input type="text" name="title" id="title" class="form-control">
-                        </div>
-                    </div>
-                </div> -->
+            <form id="form" class="form-horizontal">
+                
             </form>       
-                <button type="button" class="btn btn-success">Submit</button>               
+                <button type="button" onclick="save()" class="btn btn-success">Submit</button>               
             </div>
         </div>
     </div>
@@ -103,7 +95,7 @@ var id = "{{request()->route('id')}}";
                             var res = entry.options.split("|");
                             var g = '';
                             res.forEach(function(a) {
-                                g+='<div class="checkbox"><input type="checkbox" value='+a+' name="" id="" class=""><label>'+a+'</label></div>'
+                                g+='<div class="checkbox"><input type="checkbox" value='+a+' name="checkbox_'+i+'" id="checkbox_'+i+'" class=""><label>'+a+'</label></div>'
                             })
                             $("#form").append(
                             '<div class="form-group">'+
@@ -114,14 +106,14 @@ var id = "{{request()->route('id')}}";
                                     '</div>'+
                                 '</div>'+
                             '</div>');
-                        cnt++;
+                            cnt++;
                         }
 
                         if(entry.type=="MULTIPLE CHOICE"){
                             var res = entry.options.split("|");
                             var g = '';
                             res.forEach(function(a) {
-                                g+='<div class="checkbox"><input type="radio" value='+a+' name="r" id="" class=""><label>'+a+'</label></div>'
+                                g+='<div class="checkbox"><input type="radio" value='+a+' name="mc_'+i+'" id="mc_'+i+'" class=""><label>'+a+'</label></div>'
                             })
                             $("#form").append(
                             '<div class="form-group">'+
@@ -142,7 +134,7 @@ var id = "{{request()->route('id')}}";
                                 '<div class="form-group">'+
                                     '<label class="col-md-2 control-label" for="inputDefault">'+i+'. '+entry.question+'</label>'+
                                     '<div class="col-md-6">'+
-                                        '<input type="text" name="title" id="title" class="form-control">'+
+                                        '<input type="text" name="sa_'+i+'" id="sa_'+i+'" class="form-control">'+
                                     '</div>'+
                                 '</div>'+
                             '</div>');
@@ -156,7 +148,7 @@ var id = "{{request()->route('id')}}";
                                 '<div class="form-group">'+
                                     '<label class="col-md-2 control-label" for="inputDefault">'+i+'. '+entry.question+'</label>'+
                                     '<div class="col-md-6">'+
-                                        '<textarea name="title" id="title" class="form-control"></textarea>'+
+                                        '<textarea name="pr_'+i+'" id="pr_'+i+'" class="form-control"></textarea>'+
                                     '</div>'+
                                 '</div>'+
                             '</div>');
@@ -170,7 +162,7 @@ var id = "{{request()->route('id')}}";
                                 '<div class="form-group">'+
                                     '<label class="col-md-2 control-label" for="inputDefault">'+i+'. '+entry.question+'</label>'+
                                     '<div class="col-md-6">'+
-                                        '<textarea name="title" id="title" class="form-control"></textarea>'+
+                                        '<textarea name="sw_'+i+'" id="sw_'+i+'" class="form-control"></textarea>'+
                                     '</div>'+
                                 '</div>'+
                             '</div>');
@@ -186,23 +178,24 @@ var id = "{{request()->route('id')}}";
     }
 
     function save() {
-        var data = $('#form').serializeArray();
+        var datas = $('#form').serializeArray();
         
-        data.push({
+        /* data.push({
             name: 'mode',
             value: id?"Update":"Save"
         });
         data.push({
             name: 'cid',
             value: "{{request()->route('cid')}}"
-        });
+        }); */
+        console.log(datas)
         $.ajax({
             url: "{{ url('/admin/user_vendor/vendor_lesson_saveLesson') }}",
             type: "post",
-            data: data,
+            data: datas,
             dataType: 'JSON',
             success: function(data) {
-               location = "/admin/user_vendor/vendor_lesson_list/{{request()->route('cid')}}";
+              // location = "/admin/user_vendor/vendor_lesson_list/{{request()->route('cid')}}";
             },
             error: function(jqXHR, textStatus, errorThrown) {
                 alert('Error! Contact IT Department.');
