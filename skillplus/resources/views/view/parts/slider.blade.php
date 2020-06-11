fa96a5800def4a6ed98bd2745cf9fb2a2bffb18c
unknown <Administrator@DL-ITDPWS09.dpotmh.local>
2020-06-11 17:54:36 +0800

    ui client update

--- skillplus/resources/views/view/parts/slider.blade.php
+++ skillplus/resources/views/view/parts/slider.blade.php
@@ -1,5 +1,57 @@
+<style>
+    .carousel-indicators {
+    right: 20%;
+    left: 20%;
+}
+</style>
 <div class="container-fluid">
-    <div class="row">
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
+      
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+          <div class="item active">
+            <img class="d-block w-100"  alt="First slide [800x400]" src="/bin/admin/files/cover(7).jpg" data-holder-rendered="true">
+
+            <div class="carousel-caption">
+                <h3>lit. Repudiandae quisquam dicta, inventore consequuntur quasi accusam</h3>
+                <p>Lorem ipsum dolor sit amemus numquam ab eligendi id esse explicabo quis nesciunt odit sequi quae voluptatem mollitia corrupti officia!</p>
+            </div>
+          </div>
+
+          <div class="item">           
+            <img class="d-block w-100"  alt="First slide [800x400]" src="/bin/admin/files/cover(6).jpg" data-holder-rendered="true">
+
+            <div class="carousel-caption">
+              2
+            </div>
+          </div>
+          
+          <div class="item">           
+            <img class="d-block w-100"  alt="First slide [800x400]" src="/bin/admin/files/cover(9).jpg" data-holder-rendered="true">
+
+            <div class="carousel-caption">
+              3
+            </div>
+          </div>
+        </div>
+      
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+    </div>
+    {{-- <div class="row">
         <div class="parts-slider" style="background:url('{{{ get_option('main_page_slide','/assets/images/view/sample/slider-sample.png') }}}');">
             <div class="col-xs-12 col-md-4 col-md-offset-4 parts-slider-container">
                 <h2>{{{ get_option('main_page_slide_title','') }}}</h2>
@@ -10,6 +62,6 @@
                 </div>
             </div>
             <i class="fa fa-angle-down down-flesh"></i>
-        </div>
-    </div>
+        </div>        
+    </div> --}}    
 </div>
