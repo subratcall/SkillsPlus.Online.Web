3450ce729af899640e665f7e48c6c2cfe057729d
rsarimas <reverseedge7@gmail.com>
2020-07-14 14:59:02 +0800

    Revert "Revert "Revert "Revert "update dashboard layout,fix route redirection of dasboard""""
    This reverts commit bd1d80b3a24d8da5877e0a5d9f583ee6bb6ec662.

--- skillplus/resources/views/admin/newlayout/layout.blade.php
+++ skillplus/resources/views/admin/newlayout/layout.blade.php
@@ -297,7 +297,7 @@
       <a href="/admin">AP</a>
      </div>
      <ul class="sidebar-menu">
-      <li class="menu-header">Dashboard</li>
+      <li class="menu-header"><a href="/admin/user_dashboard/main_dashboard" class="nav-link">Dashboard </a></li>
       <li id="mp">
        <a href="/admin/profile#main" class="nav-link"><i class="fas fa-tachometer-alt"></i> <span>My Profile</span></a>
       </li>
